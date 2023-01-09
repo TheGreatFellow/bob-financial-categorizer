8397da9ddc5b023e04ee27a58694d85b805928cb
TheGreatFellow <shreyas42000@gmail.com>
2023-01-10 02:20:17 +0530

    update readme

--- src/App.jsx
+++ src/App.jsx
@@ -49,29 +49,8 @@ function App() {
     }
     return (
         <div className='App'>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src='/vite.svg' className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://reactjs.org' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={handleSubmit}>get consent</button>
-                <button onClick={handleSubmi}>data</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
+            <button onClick={handleSubmit}>get consent</button>
+            <button onClick={handleSubmi}>data</button>
         </div>
     )
 }
